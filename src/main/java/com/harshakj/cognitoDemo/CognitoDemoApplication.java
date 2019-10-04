9c380280b68bdb137c43f54a843a993da76635c0
h-hub <harsha.kj89@gmail.com>
2019-10-05 00:53:51 +0800

    first commit

---
+++ src/main/java/com/harshakj/cognitoDemo/CognitoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.harshakj.cognitoDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitoDemoApplication.class, args);
+	}
+	
+}
