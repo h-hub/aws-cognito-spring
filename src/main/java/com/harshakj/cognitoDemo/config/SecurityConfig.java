f18d0cdbe9590ba96e040b614245f0b333f8760e
h-hub <harsha.kj89@gmail.com>
2019-10-05 03:23:39 +0800

    minor fixes

--- src/main/java/com/harshakj/cognitoDemo/config/SecurityConfig.java
+++ src/main/java/com/harshakj/cognitoDemo/config/SecurityConfig.java
@@ -13,7 +13,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-         .anyRequest().authenticated()
          .and()
          .oauth2Login()
          .and().logout().logoutSuccessUrl("/").permitAll();
