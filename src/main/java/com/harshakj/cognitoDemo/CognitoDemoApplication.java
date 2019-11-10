91fa094f42f69cdd04a6eeee8a19b382d91eb94e
h-hub <harsha.kj89@gmail.com>
2019-11-10 11:27:32 +0800

    get user details from controller

--- src/main/java/com/harshakj/cognitoDemo/CognitoDemoApplication.java
+++ src/main/java/com/harshakj/cognitoDemo/CognitoDemoApplication.java
@@ -1,13 +1,29 @@
 package com.harshakj.cognitoDemo;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
 public class CognitoDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CognitoDemoApplication.class, args);
 	}
 	
+	@RequestMapping(value = "/name", method = RequestMethod.GET)
+	@ResponseBody
+    public String getName(Authentication authentication, Principal principal) {
+        System.out.println(authentication.getName());
+        System.out.println(principal.getName());
+        return authentication.getName();
+    }
 }
